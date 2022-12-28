e9618df487c31948670e0e892a7c70869eda2e59
Aditya Swain <aditya.swain@happiestminds.com>
2022-12-28 16:46:29 +0530

    bug fixes

--- src/app/cart-header/cart-header.component.ts
+++ src/app/cart-header/cart-header.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { CookieService } from 'ngx-cookie-service';
 import { CartService } from '../cart.service';
-
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-cart-header',
   templateUrl: './cart-header.component.html',
@@ -10,13 +11,18 @@ export class CartHeaderComponent implements OnInit {
 
   public totalItem : number = 0;
   public searchTerm !: string;
-  constructor(private cartService : CartService) { }
+  
+  constructor(private cartService : CartService,private cookieService:CookieService,private router: Router) { }
 
   ngOnInit(): void {
     this.cartService.getProducts()
     .subscribe(res=>{
       this.totalItem = res.length;
     })
+  }
+  deleteCookie(){
+    this.cookieService.deleteAll();
+    this.router.navigate(['/']);
   }
   search(event:any){
     this.searchTerm = (event.target as HTMLInputElement).value;
